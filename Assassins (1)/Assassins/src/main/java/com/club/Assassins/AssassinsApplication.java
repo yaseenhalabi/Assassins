475389756e6285f616238a668bc153121df8e493
Yaseen Halabi <>
2023-02-24 15:36:55 +0000

    Initial commit

---
+++ Assassins (1)/Assassins/src/main/java/com/club/Assassins/AssassinsApplication.java
@@ -0,0 +1,13 @@
+package com.club.Assassins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssassinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssassinsApplication.class, args);
+	}
+
+}
---
+++ Assassins (1)/Assassins/src/test/java/com/club/Assassins/AssassinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.Assassins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssassinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
